ebe25029d585d4aa6a1bd603a48080959164d5de
Rakesh Daniel <rakeeshdaniel321@gmail.com>
2026-06-04 17:47:03 +0530

    fix:connection

--- frontend/client/src/utils/api.js
+++ frontend/client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // 💡 VITE_API_URL கிடைக்கவில்லை என்றால், நேரடியாக போர்ட் 8000-ஐ எடுத்துக்கொள்ளும்!
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://hotel-booking-management-b130.onrender.com',
   withCredentials: true, // பேக்கெண்ட் குக்கீஸை எடுத்துச் செல்ல இது மிகக் கட்டாயம்
   headers: {
     'Content-Type': 'application/json'
